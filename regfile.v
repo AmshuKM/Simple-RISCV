c8335424eeff93d75506879c1e7ec6a73446a404
AmshuKM <110659587+AmshuKM@users.noreply.github.com>
2026-01-26 15:58:07 +0530

    Update regfile.v

--- regfile.v
+++ regfile.v
@@ -1,23 +1,24 @@
-`timescale 1ns/1ps
 module regfile (
-    input        clk,
-    input        we,
-    input  [4:0] rs1,
-    input  [4:0] rs2,
-    input  [4:0] rd,
-    input  [31:0] wd,
-    output [31:0] rd1,
-    output [31:0] rd2
+    input  wire        clk,
+    input  wire        we,
+    input  wire [4:0]  rs1,
+    input  wire [4:0]  rs2,
+    input  wire [4:0]  rd,
+    input  wire [31:0] wd,
+    output wire [31:0] rd1,
+    output wire [31:0] rd2
 );
 
     reg [31:0] regs [31:0];
 
-    assign rd1 = (rs1 == 0) ? 0 : regs[rs1];
-    assign rd2 = (rs2 == 0) ? 0 : regs[rs2];
-
     always @(posedge clk) begin
+        regs[0] <= 32'b0;  // x0 always 0
+
         if (we && rd != 0)
             regs[rd] <= wd;
     end
+
+    assign rd1 = (rs1 == 0) ? 32'b0 : regs[rs1];
+    assign rd2 = (rs2 == 0) ? 32'b0 : regs[rs2];
 
 endmodule
